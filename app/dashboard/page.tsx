e50acb38775a1b640542c48e2e902d63e59914c3
rubaiiyat <abirr1516@gmail.com>
2026-01-02 23:59:51 +0600

    conditional routing

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,10 @@
+import { redirect } from "next/navigation";
+
 export default function Dashboard() {
+  const isLogin = false;
+  if (!isLogin) {
+    redirect("/login");
+  }
   return (
     <div className="">
       <h1>This is dashboard home</h1>
